5712d781b50b99e2f6155407de40ec04eb0f3667
anapog <ana.pol.gonzalez@gmail.com>
2018-06-24 18:50:16 +0200

    Displaying recipe details

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,7 +8,11 @@ import { Recipe } from "../recipe.model";
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('This test recipe', 'This is a simple description', 'https://img.delicious.com.au/52hfFIjf/h506-w759-cfill/del/2017/05/one-pot-butter-chicken-with-dill-yoghurt-46876-2.jpg');
+    new Recipe(
+      "This test recipe",
+      "This is a simple description",
+      "https://img.delicious.com.au/52hfFIjf/h506-w759-cfill/del/2017/05/one-pot-butter-chicken-with-dill-yoghurt-46876-2.jpg"
+    )
   ];
 
   constructor() {}
